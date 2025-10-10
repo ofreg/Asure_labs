5bebbeab713039140e5103ba169ff708520b63c6
Oleksandr Matskevych <ofreg13@gmail.com>
2025-10-10 10:44:55 +0300

    Remove folder lab5

--- lab4/modules/dns_private/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "private_zone_name" {
-  value = azurerm_private_dns_zone.private_dns.name
-}
-
-output "sensor_record" {
-  value = azurerm_private_dns_a_record.sensorvm.fqdn
-}
--- lab4/modules/dns_public/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "zone_name" {
-  value = azurerm_dns_zone.public_dns.name
-}
-
-output "www_record" {
-  value = azurerm_dns_a_record.www.fqdn
-}
